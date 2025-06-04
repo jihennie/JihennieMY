6ec92a09c45137a99bbb53430f7b9afc720365f9
jihennie <a01071604586@gmail.com>
2025-06-04 17:39:16 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoadingScreen from './screens/LoadingScreen';
+import RiskInputScreen from './screens/RiskInputScreen';
+import RiskResultScreen from './screens/RiskResultScreen';
+import CheckingHealthScreen from './screens/CheckingHealthScreen';
+import MedicationMapScreen from './screens/MedicationMapScreen';
+import HealthCareGuidelinesScreen from './screens/HealthCareGuidelinesScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Loading" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Loading" component={LoadingScreen} />
+        <Stack.Screen name="RiskInput" component={RiskInputScreen} />
+        <Stack.Screen name="RiskResult" component={RiskResultScreen} />
+        <Stack.Screen name="CheckingHealth" component={CheckingHealthScreen} />
+        <Stack.Screen name="MedicationMap" component={MedicationMapScreen} />
+        <Stack.Screen name="HealthCareGuidelines" component={HealthCareGuidelinesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
